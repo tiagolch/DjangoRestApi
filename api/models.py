562633d7d528f2381ee59e3ae9347a552dfded84
Tiago <tiagolch@hotmail.com>
2020-05-02 00:05:46 -0300

    Primeiro commit

---
+++ api/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+TIPO_CONTRATO_CHOICES = {
+    ('PJ', 'Pessoa Jurídica'),
+    ('CLT', 'Consolidação das Leis do Trabalho')
+}
+
+class Vaga(models.Model):
+    titulo = models.CharField(max_length=30, null=False)
+    descricao = models.TextField(null=False)
+    salario = models.FloatField(null=False)
+    tipo_contrato = models.CharField(choices=TIPO_CONTRATO_CHOICES, null=False, max_length=50)
+    status = models.BooleanField(default=True, null=False)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        db_table = 'vaga'
